6c9d9fa08a8e0a1c42e42f93745205fe592be2c4
shiionnnnn <seanlin2987@gmail.com>
2025-03-22 16:47:22 -0500

    update

--- prompt_genai_origin_rank.py
+++ prompt_genai_origin_rank.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 client = genai.Client(api_key=os.environ.get('GOOGLE_API_KEY'))
 
-paths = "[{'originIndex': 2, 'destinationIndex': 0, 'duration': '0s'}, {'originIndex': 2, 'destinationIndex': 1, 'distanceMeters': 516, 'duration': '423s'}, {'originIndex': 0, 'destinationIndex': 1, 'distanceMeters': 1067, 'duration': '872s'}, {'originIndex': 0, 'destinationIndex': 2, 'distanceMeters': 558, 'duration': '472s'}, {'originIndex': 1, 'destinationIndex': 1, 'distanceMeters': 725, 'duration': '585s'}, {'originIndex': 0, 'destinationIndex': 0, 'distanceMeters': 1016, 'duration': '821s'}, {'originIndex': 2, 'destinationIndex': 2, 'distanceMeters': 1054, 'duration': '870s'}, {'originIndex': 1, 'destinationIndex': 0, 'distanceMeters': 1007, 'duration': '807s'}, {'originIndex': 1, 'destinationIndex': 2, 'distanceMeters': 910, 'duration': '739s'}]"
+JSONobject = "[{'originIndex': 2, 'destinationIndex': 0, 'duration': '0s'}, {'originIndex': 2, 'destinationIndex': 1, 'distanceMeters': 516, 'duration': '423s'}, {'originIndex': 0, 'destinationIndex': 1, 'distanceMeters': 1067, 'duration': '872s'}, {'originIndex': 0, 'destinationIndex': 2, 'distanceMeters': 558, 'duration': '472s'}, {'originIndex': 1, 'destinationIndex': 1, 'distanceMeters': 725, 'duration': '585s'}, {'originIndex': 0, 'destinationIndex': 0, 'distanceMeters': 1016, 'duration': '821s'}, {'originIndex': 2, 'destinationIndex': 2, 'distanceMeters': 1054, 'duration': '870s'}, {'originIndex': 1, 'destinationIndex': 0, 'distanceMeters': 1007, 'duration': '807s'}, {'originIndex': 1, 'destinationIndex': 2, 'distanceMeters': 910, 'duration': '739s'}]"
 
 class Rankings(BaseModel):
     originIndex: int
@@ -13,7 +13,7 @@ class Rankings(BaseModel):
 
 response = client.models.generate_content(
 	model="gemini-2.0-flash", 
-	contents=["Given a JSON with origin, destination, and duration of travel between the two points, rank the origins based on proximity to destinations. Output as a list of dictionaries with the origin as the index and value being the rank. Higher ranks are better. Respond in the format: [{'originIndex': #, 'rank': #}]. Do not output code.", paths],
+	contents=["Given a JSON with origin, destination, and duration of travel between the two points, rank the origins based on proximity to destinations. Output as a list of dictionaries with the origin as the index and value being the rank. Higher ranks are better. Respond in the format: [{'originIndex': #, 'rank': #}]. Do not output code.", JSONobject],
 	config = {
 		'response_mime_type': 'application/json',
 		'response_schema': list[Rankings],
